ae887bcc919c943111c841c5aacc575ef13362e2
HelenaLC <helena@crowells.eu>
2017-02-18 16:19:56 +0100

    added options "annotate" and "palette"
    Former-commit-id: c79031fcc57433e1007becadf03ef924fbc71ab1
    Former-commit-id: fc3fdeb340a0746c5127496bd9b176a8bdaf9af7

--- R/plotSpillmat.R
+++ R/plotSpillmat.R
@@ -13,12 +13,11 @@
 #' a vector of numeric masses corresponding to barcode channels.
 #' @param SM 
 #' spillover matrix returned from \code{computeSpillmat}.
-#' @param out_path 
-#' a character string. If specified, outputs will be generated in this location. 
-#' Defaults to NULL.
-#' @param name_ext 
-#' a character string. If specified, will be appended to the plot's name. 
-#' Defaults to NULL.
+#' @param annotate
+#' logical. If TRUE (default), spill percentages are shown inside bins and
+#' rows/columns are annotated with the total amount of spill caused/received. 
+#' @param palette
+#' an optional vector of colors to interpolate.
 #' 
 #' @examples
 #' data(ss_exp)
@@ -36,52 +35,59 @@
 
 # ------------------------------------------------------------------------------
 
-plotSpillmat <- function(bc_ms, SM, out_path=NULL, name_ext=NULL) {
+plotSpillmat <- function(bc_ms, SM, annotate=TRUE, palette=NULL) {
     
     nms <- colnames(SM)
     ms <- as.numeric(regmatches(nms, gregexpr("[0-9]+", nms)))
     bc_cols <- which(ms %in% bc_ms)
     bc_range <- min(bc_cols) : max(bc_cols)
-    bc_range <- which((colSums(SM) - 1) != -1)
     SM <- make_symetric(SM)[bc_range, bc_range]
     n <- length(bc_range)
-    axis_labs <- names(bc_range)
+    axis_labs <- nms[bc_range]
     lab_cols <- rep("grey", n)
     lab_cols[axis_labs %in% nms[bc_cols]] <- "black"
     
     df <- data.frame(c1=rep(1:n, n), 
-        c2=rev(rep(1:n, each=n)), 
-        spill=round(100*c(t(SM)), 1))
+                     c2=rev(rep(1:n, each=n)), 
+                     spill=round(100*c(t(SM)), 1))
     
-    pal <- colorRampPalette(c("white","lightcoral", "red2", "darkred"))(501)
-    spill_labs <- sprintf("%.1f", df$spill)
-    spill_labs[df$spill == 0 | df$spill == 100] <- ""
+    max <- ceiling(max(df$spill[df$spill != 100])/.25)*.25
     
-    row_labs <- format(round(rowSums(t(matrix(df$spill, nrow=n))) - 100, 2), digits=2)
-    col_labs <- format(round(colSums(t(matrix(df$spill, nrow=n))) - 100, 2), digits=2)
+    if (is.null(palette)) 
+        palette <- c("white","lightcoral", "red2", "darkred")
+    if (!palette[1] %in% c("white", "#FFFFFF"))
+        palette <- c("white", palette)
+    pal <- colorRampPalette(palette)(max*100)
     
-    p <- ggplot(df, aes_string(x="c1", y="c2", fill="spill")) + 
-        geom_tile(col="lightgrey") + 
-        scale_fill_gradientn(colours=pal, guide=FALSE, 
-            limits=c(0, 5), na.value="lightgrey") +
-        geom_text(aes_string(label="spill_labs"), col="black", size=2.5) +
-        annotate("text", rep(n+1.15, n), 1:n, label=rev(row_labs), 
-            fontface="bold", size=2, col=rev(lab_cols)) +
-        annotate("text", 1:n, rep(n+1, n), label=col_labs,
-            fontface="bold", size=2) +
+    p <- ggplot(df, aes_string(x="c1", y="c2")) + 
+        geom_tile(aes_string(fill="spill"), col="lightgrey") + 
+        scale_fill_gradientn(colors=pal, limits=c(0, max), guide=FALSE) +
         scale_x_discrete(limits=1:n, expand=c(0,0), labels=axis_labs) +
         scale_y_discrete(limits=1:n, expand=c(0,0), labels=rev(axis_labs)) +
-        theme_bw() + coord_fixed() + xlab(NULL) + ylab(NULL) +
-        theme(panel.grid.major=element_blank(), panel.border=element_blank(),
-            axis.text.x=element_text(vjust=.5, hjust=1, size=8, angle=90),
-            axis.text.y=element_text(vjust=.5, hjust=1, size=8, color=rev(lab_cols)))
+        coord_fixed() + xlab(NULL) + ylab(NULL) + theme_bw() + theme(
+            panel.grid.major=element_blank(), panel.border=element_blank(),
+            axis.text.x=element_text(vjust=.5, angle=90),
+            axis.text.y=element_text(vjust=.5, color=rev(lab_cols)))
+    
+    if (annotate) {
+        spill_labs <- sprintf("%.1f", df$spill)
+        spill_labs[df$spill == 0 | df$spill == 100] <- ""
+        
+        row_sums <- round(rowSums(t(matrix(df$spill, n)))-100, 2)
+        col_sums <- round(colSums(t(matrix(df$spill, n)))-100, 2)
+        row_labs <- format(row_sums, digits=2)
+        col_labs <- format(col_sums, digits=2)
+        
+        p <- p + geom_text(aes_string(label="spill_labs"), size=3) +
+        annotate("text", rep(n+1.15, n), 1:n, label=rev(row_labs), 
+                 fontface="bold", size=2.5, col=rev(lab_cols)) +
+        annotate("text", 1:n, rep(n+1, n), label=col_labs,      
+                 fontface="bold", size=2.5) 
+    }
+    
     p <- ggplot_gtable(ggplot_build(p))
     p$layout$clip[p$layout$name == "panel"] <- "off"
-    
-    if (!is.null(out_path)) {
-        ggsave(file.path(out_path, paste0("sm_heat", name_ext,".pdf")), 
-            plot=p, width=8.1, height=8)
-    } else {
-        grid.draw(p)
-    }
+    grid.draw(p)
 }
+              
+              
