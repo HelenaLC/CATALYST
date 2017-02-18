1a23e20fa7d175a72ac6cbdbfdd2cef19d5af568
HelenaLC <helena@crowells.eu>
2017-02-18 16:28:03 +0100

    added get_ids, get_deltas, get_mhl_dists
    Former-commit-id: e1c4eb537ea9d7ebcad61bbd78fa42ed3354d852
    Former-commit-id: 579cd0117a87e5d0e927e9d8d760784c5bf9995e

--- R/helpers.R
+++ R/helpers.R
@@ -1,4 +1,149 @@
 # ==============================================================================
+# returns barcode IDs 
+# called by 'assignPrelim()'
+# ------------------------------------------------------------------------------
+get_ids <- function(bcs, bc_key, ids, verbose) {
+    
+    cutoff <- 0 # used to prevent large neg. values from appearing
+                # to have sufficient separation from values near zero
+    
+    N <- nrow(bcs)
+    # order barcode intensities within ea. event
+    if (verbose) message(" o ordering")
+    bc_orders <- t(apply(bcs, 1, order, decreasing=TRUE))
+    
+    # DOUBLET-FILTERING
+    # look at k highest and (n-k)-lowest barcode channels
+    if (length(unique(rowSums(bc_key))) == 1) { 
+        
+        # number of expected pos. barcode intensities
+        n_pos_bcs <- sum(bc_key[1, ])    
+        
+        # get lowest pos. and highest neg. barcode for ea. event
+        lowest_pos  <- bcs[cbind(1:N, bc_orders[, n_pos_bcs])]
+        highest_neg <- bcs[cbind(1:N, bc_orders[, n_pos_bcs+1])]
+        
+        if (verbose) message(" o classifying events")
+        # assign binary barcode to ea. event
+        codes <- apply(bcs, 2, function(x) as.numeric(x >= lowest_pos))
+        
+        # assign barcode ID to ea. event
+        lookup <- rowSums(2 ^ col(bc_key) * bc_key)
+        preids <- rowSums(2 ^ col(codes)  * codes)
+        bc_ids <- ids[match(preids, lookup)]
+        bc_ids[is.na(bc_ids)] <- 0
+        
+        # exclude events whose pos. barcodes are still very low 
+        # (using bcs, not normalized bcs)
+        bc_ids[is.na(bc_ids) | 
+                   bcs[cbind(1:N, bc_orders[, n_pos_bcs])] < cutoff] <- 0
+        
+        # NON-CONSTANT NUMBER OF 1'S
+        # difference b/w the kth and (k–1)th highest 
+        # normalized barcode intensities
+    } else {
+        
+        # find largest barcode separation within ea. event 
+        # to assign pos. and neg. barcode values
+        diffs <- sapply(1:N, function(x) abs(diff(bcs[x, bc_orders[x, ]])))
+        largest_seps <- apply(diffs, 2, which.max)
+        pos <- sapply(1:N, function(x) bc_orders[x, 1:largest_seps[x]])
+        
+        if (verbose) message(" o classifying events")
+        # assign binary barcode to ea. event
+        codes <- t(sapply(1:N, function(x) 
+            as.numeric(1:ncol(bc_key) %in% pos[[x]])))
+        
+        # assign barcode ID to ea. event
+        lookup <- rowSums(2 ^ col(bc_key) * bc_key)
+        preids <- rowSums(2 ^ col(codes)  * codes)
+        bc_ids <- ids[match(preids, lookup)]
+        
+        # exclude events whose pos. barcodes are still very low 
+        # (using bcs, not normalized bcs)
+        pos_bcs <- sapply(1:N, function(x) bcs[x, pos[[x]]])
+        ex <- lapply(pos_bcs, function(x) any(x < cutoff))
+        bc_ids[is.na(bc_ids) | unlist(ex)] <- 0
+    }
+    bc_ids
+}
+
+# ==============================================================================
+# compute barcode separation for ea. event
+# called by 'assignPrelim()'
+# ------------------------------------------------------------------------------
+get_deltas <- function(data, bc_key, verbose) {
+    
+    N <- nrow(data)
+    # order barcode intensities within ea. event
+    bc_orders <- t(apply(data, 1, order, decreasing=TRUE))
+    
+    # DOUBLET-FILTERING
+    # look at k highest and (n-k)-lowest barcode channels
+    if (length(unique(rowSums(bc_key))) == 1) { 
+        
+        # number of expected pos. barcode intensities
+        n_pos_bcs <- sum(bc_key[1, ])    
+        
+        # get lowest pos. and highest neg. barcode for ea. event
+        lowest_pos  <- data[cbind(1:N, bc_orders[, n_pos_bcs])]
+        highest_neg <- data[cbind(1:N, bc_orders[, n_pos_bcs+1])]
+        
+        # compute separation b/w pos. and neg. barcodes for ea. event
+        deltas <- lowest_pos - highest_neg
+        
+        # NON-CONSTANT NUMBER OF 1'S
+        # difference b/w the kth and (k–1)th highest 
+        # normalized barcode intensities
+    } else {
+        
+        # find largest barcode separation within ea. event 
+        # to assign pos. and neg. barcode values
+        diffs <- sapply(1:N, function(x) abs(diff(data[x, bc_orders[x, ]])))
+        largest_seps <- apply(diffs, 2, which.max)
+        deltas <- sapply(1:N, function(x) diffs[largest_seps[x], x])
+    }
+    deltas
+}
+
+# ==============================================================================
+# computes mahalanobis distances given current separation cutoffs
+# called by 'assignPrelim()'
+# ------------------------------------------------------------------------------
+get_mhl_dists <- function(x) {
+    # get channel and barcode masses
+    nms <- colnames(x@exprs)
+    ms <- as.numeric(regmatches(nms, gregexpr("[0-9]+", nms)))
+    
+    # find which columns correspond to barcode masses
+    bc_cols <- which(ms %in% colnames(x@bc_key))
+    n_bcs <- length(bc_cols)
+    
+    ids <- unique(x@bc_ids)
+    ids <- sort(ids[which(ids != 0)])
+    
+    # extract barcode columns from FCS file
+    bcs <- x@exprs[, bc_cols]
+    
+    # compute mahalanobis distances of all events
+    # given current separation cutoff
+    mhl_dists <- numeric(nrow(x@exprs))
+    for (i in ids) {
+        inds <- which(x@bc_ids == i)
+        ex <- inds[x@deltas[inds] < x@sep_cutoffs[rownames(x@bc_key) == i]]
+        inds <- inds[!(inds %in% ex)]
+        x@bc_ids[ex] <- 0
+        sub  <- bcs[inds, ]
+        if (length(sub) != n_bcs)
+            if (nrow(sub) > n_bcs)
+                mhl_dists[inds] <- stats::mahalanobis(
+                    x=sub, center=colMeans(sub), cov=stats::cov(sub))
+    }
+    x@mhl_dists <- mhl_dists
+    x
+}
+
+# ==============================================================================
 # retrieve legend from ggplot
 # ------------------------------------------------------------------------------
 get_legend <- function(p) {
