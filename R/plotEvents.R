0d4d09dab5c270ed5479a22deeacadeec034e0c6
HelenaLC <helena@crowells.eu>
2017-02-19 11:53:25 +0100

    validity check 'n_events', throw warning about populations with less than 50 event assignments
    Former-commit-id: b8b81e9a722e17743d7df44f15598e9347d65f02
    Former-commit-id: 5d6a27342057b68aa81632023179edbe275849e1

--- R/plotEvents.R
+++ R/plotEvents.R
@@ -54,89 +54,85 @@ setMethod(f="plotEvents",
         
         # ----------------------------------------------------------------------
         # check validity of function arguments
-        if (!"all" %in% which) 
+        if ("all" %in% which & length(which) > 1) {
+            warning("'which' must either \"all\", or a numeric or character\n",
+                "corresponding to valid barcode IDs; ",
+                "using default value \"all\".", call.=FALSE)
+        } else {
             which <- check_validity_which(
                 which, rownames(x@bc_key), "events")
+        }
         if (!is.numeric(n_events) || n_events == 0 || length(n_events) > 1) {
             warning("'n_events' must be a numeric greater than 0 and ",
-                "of length one;\n using default value 100.", call.=FALSE)
+                "of length one;\nusing default value 100.", call.=FALSE)
             n_events <- 100
         }
         # ----------------------------------------------------------------------
-        
-        set.seed(8)
-        
-        nms <- colnames(x@exprs)
-        ms <- as.numeric(regmatches(nms, gregexpr("[0-9]+", nms)))
-        
-        ids <- sort(unique(x@bc_ids))
-        n_bcs <- nrow(x@bc_key)
+
         n_chs <- ncol(x@bc_key)
-        
-        if (sum(rowSums(x@bc_key) == 1) == n_bcs) {
-            bc_labs <- paste(colnames(x@exprs))[
-                ms %in% as.numeric(colnames(x@bc_key))]
+        ids <- sort(unique(x@bc_ids))
+        if ("all" %in% which) which <- ids
+
+        # get barcode labels: 
+        # channel name if barcodes are single-positive, 
+        # barcode ID and binary code elsewise 
+        if (sum(rowSums(x@bc_key) == 1) == nrow(x@bc_key)) {
+            nms <- colnames(x@exprs)
+            ms <- as.numeric(gsub("[[:alpha:][:punct:]]", "", nms))
+            bc_labs <- paste(nms[ms %in% colnames(x@bc_key)])
         } else {
             bc_labs <- paste0(rownames(x@bc_key), ": ", 
                 apply(x@bc_key, 1, function(x) paste(x, collapse="")))
         }
-        if (0 %in% ids) 
-            bc_labs <- c("Unassigned", bc_labs)
-        bc_labs <- bc_labs[c(0, rownames(x@bc_key)) %in% ids]
+        if (0 %in% ids) bc_labs <- c("Unassigned", bc_labs)
         
-        labs <- colnames(x@normed_bcs)
+        # get colors for plotting:
+        # interpolate if more than 11 barcodes, 
+        # use colors spaced equally along palette elsewise
         pal <- RColorBrewer::brewer.pal(11,"Spectral")
         if (n_chs > 11) {
             cols <- colorRampPalette(pal)(ncol(x@normed_bcs))
         } else {
             cols <- pal[ceiling(seq(1, 11, length=ncol(x@normed_bcs)))] 
         }
-        thms <- theme_bw() + theme(legend.key=element_blank(),
-            panel.grid.major=element_line(color="lightgrey"),
-            panel.grid.minor=element_blank())
         
-        if ("all" %in% which) {
-            which_ids <- ids
-        } else {
-            which_ids <- which
-        }
-        
-        # initialize vector to store IDs with no or insufficient events assigned
         skipped <- NULL
-        
         p <- list()
-        for (id in which_ids) {
-            inds <- which(x@bc_ids == id)
-            n <- length(inds)
-            if (length(inds) < 50) {
+        for (id in which) {
+            inds <- x@bc_ids == id
+            n <- sum(inds)
+            # store IDs with no or insufficient events assigned
+            if (sum(inds) < 50) {
                 skipped <- c(skipped, id)
                 next
             }
-            if (length(inds) > n_events) 
-                inds <- sort(sample(inds, n_events))
-            psize <- 1 + 100 / length(inds)
-            sub <- x@normed_bcs[inds, ]
-            
-            tcks <- c(1,nrow(sub))
-            ymin <-   floor(4 * min(sub)) / 4
-            ymax <- ceiling(4 * max(sub)) / 4
-            
+            # subsample events if more than 'n_events' assigned 
+            if (sum(inds) > n_events) 
+                inds <- sort(sample(which(inds), n_events))
+            # use normalized barcode intensities
+            ints <- x@normed_bcs[inds, ]
+
             df <- data.frame(
-                event=rep(1:(length(sub) / n_chs), each=n_chs),
-                intensity=c(t(sub)),
-                bc=rep(1:n_chs, (length(sub) / n_chs)))
+                event=rep(1:(length(ints) / n_chs), each=n_chs),
+                intensity=c(t(ints)),
+                bc=rep(1:n_chs, (length(ints) / n_chs)))
 
             p[[length(p) + 1]] <- ggplot(df) + 
-                geom_point(stroke=.5, size=psize, aes_string(
+                geom_point(stroke=.5, size=1+100/sum(inds), aes_string(
                     x="event", y="intensity", col="as.factor(bc)", alpha=.8)) +
-                scale_colour_manual(values=cols, name=NULL, labels=labs) + 
+                scale_colour_manual(values=cols, name=NULL, 
+                    labels=colnames(x@normed_bcs)) + 
                 guides(alpha=FALSE, 
                     colour=guide_legend(override.aes=list(size=3))) +
-                scale_x_discrete(limits=NULL, labels=NULL) + ylim(ymin, ymax) + 
-                expand_limits(x=c(0, nrow(sub)+1)) +
-                xlab("Event number") + ylab("Normalized intensity") + thms +
+                scale_x_discrete(limits=NULL, labels=NULL) + 
+                expand_limits(x=c(0, nrow(ints)+1)) +
+                ylim(floor(4*min(ints))/4, ceiling(4*max(ints))/4) + 
+                xlab("Event number") + ylab("Normalized intensity") + 
                 ggtitle(bquote(bold(.(bc_labs[ids == id]))*
-                        scriptstyle(" ("*.(n)*" events)")))
+                        scriptstyle(" ("*.(n)*" events)"))) +
+                theme_bw() + theme(legend.key=element_blank(),
+                    panel.grid.major=element_line(color="lightgrey"),
+                    panel.grid.minor=element_blank())
         }
         
         # throw informative warning about populations with 
