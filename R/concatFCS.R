f0c2d9499f91a5a0508eef8a49da499e568fa844
HelenaLC <helena@crowells.eu>
2017-03-16 15:56:33 +0100

    + concatFCS()

--- R/concatFCS.R
+++ R/concatFCS.R
@@ -21,54 +21,52 @@
 #' the measured intensities of all provided FCS files. Files will be 
 #' linked together in the order of appearance in the specified location.
 #' 
-#' @examples
-#' 
 #' @author Helena Lucia Crowell \email{crowellh@student.ethz.ch}
 #' @importFrom flowCore flowFrame colnames exprs 
 #' @export
 # ==============================================================================
- 
+
 setMethod(f="concatFCS",
     signature=signature(x="character"),
     definition=function(x, y=NULL) {     
-    
-    fcs <- list.files(x, ".fcs", full.names=TRUE, ignore.case=TRUE)
-    n <- length(fcs)
-    if (n < 2) {
-        if (n == 0) stop("No FCS files have been found in \"", x, "\"")
-        if (n == 1) stop("Only a single FCS file has been found in \"", 
-            x, "\"\n  - No concatination required!")
-    }
-    
-    ffs <- lapply(fcs, flowCore::read.FCS)
-    chs <- lapply(ffs, flowCore::colnames)
-    for (i in 2:n) 
-        if (!isTRUE(all.equal(chs[[1]], chs[[i]])))
-            stop("\n  Channel names of flowFrame ", i, 
-                " don't match those of flowFrame 1.\n ",
-                " Please make sure all FCS files share the same",
-                " measurement parameters!")
-    
-    n_events <- unlist(lapply(ffs, function(k) nrow(k)))
-    exprs <- lapply(ffs, function(k) flowCore::exprs(k))
-    time_col <- grep("time", colnames(exprs[[1]]), TRUE)
-    times <- lapply(exprs, function(k) k[, time_col])
-    
-    new_times <- times[[1]]
-    for (i in 2:n) 
-        new_times <- append(new_times, times[[i]]+max(new_times))
-    new_es <- do.call(rbind, exprs)
-    new_es[, time_col] <- new_times
-    ff <- new("flowFrame", exprs=new_es, 
-        parameters=flowCore::parameters(ffs[[1]]), 
-        description=flowCore::description(ffs[[1]]))
-    
-    if (is.null(y)) {
-        return(ff)
-    } else {
-        suppressWarnings(flowCore::write.FCS(ff, 
-            file.path(y, paste0(gsub("_\\d+.fcs", "",
-                list.files(x, ".fcs", ignore.case=TRUE)[[1]], 
-                ignore.case=TRUE), "_concat.fcs"))))
-    }
-})
+        
+        fcs <- list.files(x, ".fcs", full.names=TRUE, ignore.case=TRUE)
+        n <- length(fcs)
+        if (n < 2) {
+            if (n == 0) stop("No FCS files have been found in \"", x, "\"")
+            if (n == 1) stop("Only a single FCS file has been found in \"", 
+                x, "\"\n  - No concatination required!")
+        }
+        
+        ffs <- lapply(fcs, flowCore::read.FCS)
+        chs <- lapply(ffs, flowCore::colnames)
+        for (i in 2:n) 
+            if (!isTRUE(all.equal(chs[[1]], chs[[i]])))
+                stop("\n  Channel names of flowFrame ", i, 
+                    " don't match those of flowFrame 1.\n ",
+                    " Please make sure all FCS files share the same",
+                    " measurement parameters!")
+        
+        n_events <- unlist(lapply(ffs, function(k) nrow(k)))
+        exprs <- lapply(ffs, function(k) flowCore::exprs(k))
+        time_col <- grep("time", colnames(exprs[[1]]), TRUE)
+        times <- lapply(exprs, function(k) k[, time_col])
+        
+        new_times <- times[[1]]
+        for (i in 2:n) 
+            new_times <- append(new_times, times[[i]]+max(new_times))
+        new_es <- do.call(rbind, exprs)
+        new_es[, time_col] <- new_times
+        ff <- new("flowFrame", exprs=new_es, 
+            parameters=flowCore::parameters(ffs[[1]]), 
+            description=flowCore::description(ffs[[1]]))
+        
+        if (is.null(y)) {
+            return(ff)
+        } else {
+            suppressWarnings(flowCore::write.FCS(ff, 
+                file.path(y, paste0(gsub("_\\d+.fcs", "",
+                    list.files(x, ".fcs", ignore.case=TRUE)[[1]], 
+                    ignore.case=TRUE), "_concat.fcs"))))
+        }
+    })
