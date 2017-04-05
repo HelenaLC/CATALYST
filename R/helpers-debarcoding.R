80d90944e822a3263e7b18af75da2f615976bc88
HelenaLC <helena@crowells.eu>
2017-04-05 10:06:00 +0200

    change 1:N -> seq_len(N)

--- R/helpers-debarcoding.R
+++ R/helpers-debarcoding.R
@@ -20,8 +20,8 @@ get_ids <- function(bcs, bc_key, ids, verbose) {
         n_pos_bcs <- sum(bc_key[1, ])    
         
         # get lowest pos. and highest neg. barcode for ea. event
-        lowest_pos  <- bcs[cbind(1:N, bc_orders[, n_pos_bcs])]
-        highest_neg <- bcs[cbind(1:N, bc_orders[, n_pos_bcs+1])]
+        lowest_pos  <- bcs[cbind(seq_len(N), bc_orders[, n_pos_bcs])]
+        highest_neg <- bcs[cbind(seq_len(N), bc_orders[, n_pos_bcs+1])]
         
         if (verbose) message(" o classifying events")
         # assign binary barcode to ea. event
@@ -35,8 +35,8 @@ get_ids <- function(bcs, bc_key, ids, verbose) {
         
         # exclude events whose pos. barcodes are still very low 
         # (using bcs, not normalized bcs)
-        bc_ids[is.na(bc_ids) | 
-                bcs[cbind(1:N, bc_orders[, n_pos_bcs])] < cutoff] <- 0
+        ex <- bcs[cbind(seq_len(N), bc_orders[, n_pos_bcs])] < cutoff
+        bc_ids[is.na(bc_ids) || ex] <- 0
         
         # NON-CONSTANT NUMBER OF 1'S
         # difference b/w the kth and (k–1)th highest 
@@ -45,14 +45,16 @@ get_ids <- function(bcs, bc_key, ids, verbose) {
         
         # find largest barcode separation within ea. event 
         # to assign pos. and neg. barcode values
-        diffs <- sapply(1:N, function(x) abs(diff(bcs[x, bc_orders[x, ]])))
+        diffs <- sapply(seq_len(N), function(x) 
+            abs(diff(bcs[x, bc_orders[x, ]])))
         largest_seps <- apply(diffs, 2, which.max)
-        pos <- sapply(1:N, function(x) bc_orders[x, 1:largest_seps[x]])
+        pos <- sapply(seq_len(N), function(x) 
+            bc_orders[x, 1:largest_seps[x]])
         
         if (verbose) message(" o classifying events")
         # assign binary barcode to ea. event
-        codes <- t(sapply(1:N, function(x) 
-            as.numeric(1:ncol(bc_key) %in% pos[[x]])))
+        codes <- t(sapply(seq_len(N), function(x) 
+            as.numeric(seq_len(ncol(bc_key)) %in% pos[[x]])))
         
         # assign barcode ID to ea. event
         lookup <- rowSums(2 ^ col(bc_key) * bc_key)
@@ -61,7 +63,7 @@ get_ids <- function(bcs, bc_key, ids, verbose) {
         
         # exclude events whose pos. barcodes are still very low 
         # (using bcs, not normalized bcs)
-        pos_bcs <- sapply(1:N, function(x) bcs[x, pos[[x]]])
+        pos_bcs <- sapply(seq_len(N), function(x) bcs[x, pos[[x]]])
         ex <- lapply(pos_bcs, function(x) any(x < cutoff))
         bc_ids[is.na(bc_ids) | unlist(ex)] <- 0
     }
