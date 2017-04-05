9dc87112c07bedb66c829b78988617dbef2fcd06
HelenaLC <helena@crowells.eu>
2017-04-05 09:37:07 +0200

    change object -> x and @ -> accessor

--- R/show-methods.R
+++ R/show-methods.R
@@ -4,41 +4,41 @@
 
 setMethod(f="show",
     signature="dbFrame",
-    definition=function(object) {
-        dims <- dim(object@exprs)
+    definition=function(x) {
+        dims <- dim(exprs(x))
         cat("dbFrame objectect with\n", 
             dims[1], " events, ", 
             dims[2], " observables and ",
-            nrow(object@bc_key), " barcodes:\n", sep="")
+            nrow(bc_key(x)), " barcodes:\n", sep="")
         
         cat("\nCurrent assignments:\n")
-        cat("     ", sum(object@bc_ids == 0), "event(s) unassigned")
+        cat("     ", sum(bc_ids(x) == 0), "event(s) unassigned")
         
-        tmp <- c(sort(table(object@bc_ids[object@bc_ids != 0]), 
+        tmp <- c(sort(table(bc_ids(x)[bc_ids(x) != 0]), 
             decreasing=TRUE))
         tbl <- t(data.frame("ID"=names(tmp), "Count"=tmp))
         dimnames(tbl)[2] <- list(rep("", dim(tbl)[2]))
         print(noquote(tbl))
         
-        if (length(object@sep_cutoffs) != 0) {
-            inds <- match(names(tmp), rownames(object@bc_key))
+        if (length(sep_cutoffs(x)) != 0) {
+            inds <- match(names(tmp), rownames(bc_key(x)))
             cat("\nSeparation cutoffs:")
             tbl <- t(data.frame(
                 "ID"=names(tmp), 
-                "Cutoff"=object@sep_cutoffs[inds]))
+                "Cutoff"=sep_cutoffs(x)[inds]))
             dimnames(tbl)[2] <- list(rep("", dim(tbl)[2]))
             print(noquote(tbl))
             
             # better way to do this?
             seps <- seq(0, 1, .01)
             ind <- NULL
-            for (i in object@sep_cutoffs[inds]) {
+            for (i in sep_cutoffs(x)[inds]) {
                 for (j in seps) {
                     if (isTRUE(all.equal(i, j))) 
                         ind <- append(ind, which(seps == j))
                 }
             }
-            yields <- object@yields[cbind(inds, ind)]
+            yields <- yields(x)[cbind(inds, ind)]
             cat("\nYields upon debarcoding:\n")
             cat("     ", paste0(round(mean(yields), 4) * 100, "%"), 
                 "overall yield")
