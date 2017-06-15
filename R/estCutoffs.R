ba6a5dfa193d10f0d2ff2f6d6e5d0877fcbcc857
h.crowell <h.crowell@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-06-15 07:57:51 +0000

    bug fix
    From: HelenaLC <helena@crowells.eu>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CATALYST@130398 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/estCutoffs.R
+++ R/estCutoffs.R
@@ -42,25 +42,17 @@ setMethod(f="estCutoffs",
         # f2 <- function(x, b, c, d, e) b*e^b*x^(b-2)*((b-1)*e^b-(b+1)*x^b)*(c-d)/(x^b+e^b)^3
         # f3 <- function(x, b, c, d, e) (b*e^b*x^(b-3)*(b^2+3*b+2)*x^(2*b)-4*(b^2-1)*e^b*x^b+(b^2-3*b+2)*e^(2*b)*(c-d))/(x^b+e^b)^4
         f4 <- function(x, b, c, d, e) (b*(c-d)*e^b*x^(-4+b)*((-6+11*b-6*b^2+b^3)*e^(3*b)+(-18+11*b+18*b^2-11*b^3)*e^(2*b)*x^b+(-18-11*b+18*b^2+11*b^3)*e^b*x^(2*b)-(6+11*b+6*b^2+b^3)*x^(3*b)))/(e^b+x^b)^5
-        # root <- function(b, c, d, e) 
-        #     ((2*e^b*b^2-sqrt(3)*sqrt(b^4*e^(2*b)-b^2*e^(2*b))
-        #         -2*e^b)/(b^2+3*b+2))^(1/b)
         
         for (i in seq_len(n_bcs)) {
             df <- data.frame(cutoff=cutoffs, yield=as.vector(yields(x)[i, ]))
-            fit <- drc::drm(yield~cutoff, data=df, fct=drc::LL.4(fixed=c(NA, 0, NA, NA)))
-            #ests[i] <- round(root(fit$coefficients[1], 0, 
-            #    fit$coefficients[2], fit$coefficients[3])*100)/100
+            fit <- drc::drm(yield~cutoff, data=df, 
+                fct=drc::LL.4(fixed=c(NA, 0, NA, NA)))
             b <- fit$coefficients[1]
-            c <- 0#fit$coefficients[2]
+            c <- 0
             d <- fit$coefficients[2]
             e <- fit$coefficients[3]
             root <- which(diff(sign(diff(f4(cutoffs, b, c, d, e)))) == 2) + 1
             ests[i] <- cutoffs[root[1]]
-            #ests[i] <- round(root(fit$coefficients[1], fit$coefficients[3])*100)/100
-            # ests[i] <- round(root(
-            #     fit$coefficients[1], fit$coefficients[2], 
-            #     fit$coefficients[3], fit$coefficients[4])*100)/100
         }
         
         names(ests) <- rownames(bc_key(x))
