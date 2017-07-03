ca64c12c55b93cbc509db175dbd273edd14efcba
h.crowell <h.crowell@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-07-03 09:20:14 +0000

    update devel
    From: HelenaLC <helena@crowells.eu>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CATALYST@130885 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/shinyGUI/ui/aes.R
+++
@@ -1,7 +0,0 @@
-inline       <- "display:inline-block;"
-inlineTop    <- "display:inline-block; vertical-align:top"
-inlineTop34  <- "display:inline-block; vertical-align:top; height:34px"
-inlineCenter <- "display:inline-block; vertical-align:center"
-
-collapseBox <- "shinyjs.collapse=function(boxId){
-    $('#'+boxId).closest('.box').find('[data-widget=collapse]').click();}"
