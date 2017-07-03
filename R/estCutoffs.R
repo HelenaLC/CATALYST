0f587408907af31602e10a76b6249dbf57278ddb
h.crowell <h.crowell@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-07-03 09:18:21 +0000

    update doc, add stats::D to imports
    From: HelenaLC <helena@crowells.eu>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CATALYST@130874 bc3139a8-67e5-0310-9ffc-ced21a209358
    
    Former-commit-id: adfdd2dc30c8233b9898ffba0de8ff1402a7a00f
    Former-commit-id: 1cd6bc8889461503f3218c7b47220a82914c7d8c

--- R/estCutoffs.R
+++ R/estCutoffs.R
@@ -12,7 +12,7 @@
 #' @param x       
 #' a \code{\link{dbFrame}}.
 #' @param deriv
-#' a single numeric between 2 and 8. Specifies which derivative of the 
+#' a single positive integer. Specifies which derivative of the 
 #' three-parameter log-logistic function fit to use for cutoff estimation 
 #' (see below for more details). 
 #' Generally, a low value will yield more stringent (higher) estimates, 
@@ -24,14 +24,15 @@
 #' For the estimation of sample-specific cutoff parameters, we fit a 
 #' three-parameter log-logistic function to the yields function. 
 #' As an adequate cutoff estimate, we target a point which approximately marks 
-#' the end of the plateu regime and on-set of yield decline. By default,
-#' we compute this as the first minimum of the fifth derivative (\code{d=5}). 
-#' However, depending on the overall doublet-singlet separation, 
-#' and how bimodel the count distribution is, another derivate may provide 
-#' a better estimate. As a general guideline, higher values of \code{d} 
-#' will shift the computed minimum towards the left and yield more liberal 
-#' (low) cutoffs, while a low \code{d} will shift it towards the inflection
-#' point of the yields functions, arriving at more stringent (high) estimates.
+#' the end of the plateau regime and on-set of yield decline. 
+#' By default, we compute this as the first minimum of the fifth derivative 
+#' (\code{deriv=5}). However, depending on the overall doublet-singlet 
+#' separation, and how bimodel the count distribution is, another derivate 
+#' may provide a better estimate. 
+#' As a general guideline, higher values of \code{deriv} will shift the 
+#' computed minimum towards the left and yield more liberal (low) cutoffs, 
+#' while a low \code{deriv} will shift it towards the inflection point of 
+#' the yields functions, arriving at more stringent (high) estimates.
 #'
 #' @return
 #' Will update the \code{sep_cutoffs} slot of the input \code{\link{dbFrame}} 
@@ -42,14 +43,14 @@
 #' # assign preliminary IDs
 #' re <- assignPrelim(x = sample_ff, y = sample_key)
 #' # estimate separation cutoffs
-#' re_d4 <- estCutoffs(x = re, d = 4)
-#' re_d5 <- estCutoffs(x = re, d = 5)
+#' re_d4 <- estCutoffs(x = re, deriv = 4)
+#' re_d5 <- estCutoffs(x = re, deriv = 5)
 #' # view exemplary estimates
 #' plotYields(re_d4, "A1")
 #' plotYields(re_d5, "A1")
 #'
 #' @author Helena Lucia Crowell \email{crowellh@student.ethz.ch}
-#' @importFrom stats deriv
+#' @importFrom stats D
 #' @importFrom drc drm LL.3
 
 # ------------------------------------------------------------------------------
@@ -58,14 +59,18 @@ setMethod(f="estCutoffs",
     signature=signature(x="dbFrame"), 
     definition=function(x, deriv=5) {
         
+        if (!is.numeric(deriv) || deriv < 1)
+            stop("Invalid 'deriv' specified; 
+                should be a single positive integer.")
+        
         sep_cutoffs <- seq(0, 1, .01)
         n_bcs <- nrow(bc_key(x))
         ests <- numeric(n_bcs)
         # get dth derivative of three-parameter
         # log-logistic function
         llf <- quote(d/(1+exp(b*(log(sep_cutoffs)-log(e)))))
-        for (i in seq_len(deriv))
-            llf <- D(llf, "sep_cutoffs")
+        for (i in seq_len(deriv)) 
+            llf <- stats::D(llf, "sep_cutoffs")
 
         for (i in seq_len(n_bcs)) {
             df <- data.frame(x=sep_cutoffs, y=as.vector(yields(x)[i, ]))
@@ -74,7 +79,6 @@ setMethod(f="estCutoffs",
             d <- fit$coefficients[2]
             e <- fit$coefficients[3]
             root <- which(diff(sign(diff(eval(llf)))) == 2) + 1
-            #root <- which(diff(sign(diff(llf(cutoffs, b, d, e)))) == 2) + 1
             ests[i] <- sep_cutoffs[root[1]]
         }
         
