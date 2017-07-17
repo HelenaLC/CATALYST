754df5752086b10f3f91bf781cde91355cc55f8c
HelenaLC <helena@crowells.eu>
2017-07-17 14:56:31 +0200

    update Shiny

--- inst/shinyGUI/ui/aes.R
+++ inst/shinyGUI/ui/aes.R
@@ -4,4 +4,4 @@ inlineTop34  <- "display:inline-block; vertical-align:top; height:34px"
 inlineCenter <- "display:inline-block; vertical-align:center"
 
 collapseBox <- "shinyjs.collapse=function(boxId){
-    $('#'+boxId).closest('.box').find('[data-widget=collapse]').click();}"
+    $('#'+boxId).closest('.box').not('.collapsed-box').find('[data-widget=collapse]').click();}"
