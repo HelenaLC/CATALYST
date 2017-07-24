e5678ff22cacacbf5befd5065e67cb261a105cfc
HelenaLC <helena@crowells.eu>
2017-07-24 08:34:48 +0200

    bug fix

--- inst/shinyGUI/global.R
+++ inst/shinyGUI/global.R
@@ -11,14 +11,14 @@ library(flowCore)
 library(magrittr)
 library(DT)
 
-source("ui-normalization.R")   
-source("ui-debarcoding.R")
-source("ui-compensation.R")
-
-source("module-yieldPlot.R")
-source("module-debaPars.R")
-
 # helper to force collapse shinydashboard::box
 collapseBox <- "shinyjs.collapse=function(boxId){
 $('#'+boxId).closest('.box').not('.collapsed-box')
 .find('[data-widget=collapse]').click();}"
+
+source("module-yieldPlot.R")
+source("module-debaPars.R")
+
+source("ui-normalization.R")   
+source("ui-debarcoding.R")
+source("ui-compensation.R")
