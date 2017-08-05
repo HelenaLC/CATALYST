7058c0b2e6c6751f38e103947d4e49652c2a01e5
votti <vito.zanotelli@gmail.com>
2017-08-05 10:22:20 +0000

    Adds the first test for comp cytof. However currently the test fails due to a bug in compcytof.
    Former-commit-id: 8dc533c237c5f402ed5f6a2ff56e8a7d97ad9af4
    Former-commit-id: 0aa9f023259a7a7072618d1711933551dfab1565

---
+++ tests/testthat/test_compcytof.R
@@ -0,0 +1,30 @@
+test_that("compCytof() works.", {
+  # Tests if artificail added spillover can be removed
+  data(ss_exp)
+
+  # generate a dummy spillover matrix
+  ncol = flowCore::ncol(ss_exp)
+  sm = diag(1, ncol, ncol)
+  sm[cbind(1:(ncol-1),2:(ncol))] = 0.1
+  colnames(sm) <- rownames(sm) <- colnames(ss_exp)
+  
+  # add the spillover
+  ss_morespill = ss_exp
+  flowCore::exprs(ss_morespill)= flowCore::exprs(ss_exp) %*% sm
+
+  # compensate
+  ss_morespill_comp = CATALYST::compCytof(ss_morespill, sm)
+  
+  # check the data
+  dat_orig = flowCore::exprs(ss_exp)
+  dat_spill =  flowCore::exprs(ss_morespill)
+  dat_corr = flowCore::exprs(ss_morespill)
+  # to test if the spillover addition was correct, the data gets also compensated using
+  # flowcore directly
+  dat_corr_flowcore = flowCore::exprs(flowCore::compensate(ss_morespill,sm))
+  
+  testthat::expect_equal(dat_orig, dat_corr_flowcore, tolerance=10^-10, info = 'Tests if the spillover was correctly simulated.')
+  
+  testthat::expect_equal(dat_orig, dat_corr, tolerance=10^-10, info = 'Tests if the compCytof corrects artificial spillover.')
+
+})
