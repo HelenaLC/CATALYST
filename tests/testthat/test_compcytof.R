859866e74e34d72b02c7b9260ac30ddaab78ecee
votti <vito.zanotelli@gmail.com>
2017-08-05 18:06:41 +0000

    rewrites parts of compytof tests adds adaptCompensationSpillmat tests

--- tests/testthat/test_compcytof.R
+++ tests/testthat/test_compcytof.R
@@ -6,19 +6,19 @@ test_that("compCytof() works.", {
   ncol = flowCore::ncol(ss_exp)
   sm = diag(1, ncol, ncol)
   sm[cbind(1:(ncol-1),2:(ncol))] = 0.1
-  colnames(sm) <- rownames(sm) <- colnames(ss_exp)
+  colnames(sm) <- rownames(sm) <- flowCore::colnames(ss_exp)
   
   # add the spillover
   ss_morespill = ss_exp
-  flowCore::exprs(ss_morespill)= flowCore::exprs(ss_exp) %*% sm
-
+  flowCore::exprs(ss_morespill)  = flowCore::exprs(ss_exp) %*% (sm)
   # compensate
   ss_morespill_comp = CATALYST::compCytof(ss_morespill, sm)
   
+  s = adaptCompensationSpillmat(sm, flowCore::colnames(ss_morespill)) 
   # check the data
   dat_orig = flowCore::exprs(ss_exp)
   dat_spill =  flowCore::exprs(ss_morespill)
-  dat_corr = flowCore::exprs(ss_morespill)
+  dat_corr = flowCore::exprs(ss_morespill_comp)
   # to test if the spillover addition was correct, the data gets also compensated using
   # flowcore directly
   dat_corr_flowcore = flowCore::exprs(flowCore::compensate(ss_morespill,sm))
