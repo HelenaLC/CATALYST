e6286ecbdecd32ef6f1454706a40ae0ac0e9ca94
HelenaLC <helena@crowells.eu>
2017-08-09 13:18:09 +0200

    add FCS file checking, make yield plots interactive

--- inst/shinyGUI/global.R
+++ inst/shinyGUI/global.R
@@ -11,25 +11,13 @@ library(flowCore)
 library(magrittr)
 library(DT)
 
-# helper to style textInput
-restyleTextInputConcat <- "shinyjs.restyleTextInputConcat=function(id){
-    $('#'+id).css('background-color','#e8effc')
-    .css('height','28px').css('line-height','12px')
-    .css('padding-top','6px').css('padding-bottom','6px')
-    .css('margin-bottom','-6px').css('border','0px')
-    .css('font-size','16px').css('color','navy');}"
+# change background color of textInput
+restyleMetals <- "shinyjs.restyleMetal=function(pars){
+    $('#'+pars.id).css('background-color',pars.col);}"
 
-# helper to style textInput
-styleTextInput <- 'shinyjs.textInputCol=function(pars){
-    $("#"+pars.id).css("background-color",pars.col)
-    .css("height","24px").css("line-height","12px")
-    .css("padding-top","6px").css("padding-bottom","6px")
-    .css("margin-bottom","-8px").css("border","0px")
-    .css("font-size","12px").css("color","black");}'
-
-# helper to force collapse shinydashboard::box
+# force collapse shinydashboard::box
 collapseBox <- "shinyjs.collapse=function(boxId){
-    $('#'+boxId).closest('.box').not('.collapsed-box')
+    $('#'+id).closest('.box').not('.collapsed-box')
     .find('[data-widget=collapse]').click();}"
 
 source("helpers.R")
