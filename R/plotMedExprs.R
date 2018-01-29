65f6ee0bf1e81ab9a74e347366ec9480373f78de
HelenaLC <crowellh@student.ethz.ch>
2018-01-29 14:28:04 +0100

    updt
    Former-commit-id: 9233dc6b7b8e5f26082a5ddb78726da1ca413d45
    Former-commit-id: 192da07e33299b59a5dc735cf206d71276cee6f6
    Former-commit-id: 3f24d5c332092ed36d6fdfb03d49ff68fbccd87d
    Former-commit-id: 41edef0305ac6c34f5d873a9ab66e5d0e4010429

--- R/plotMedExprs.R
+++ R/plotMedExprs.R
@@ -14,7 +14,11 @@
 #' @examples
 #' data(PBMC_fs, PBMC_panel, PBMC_md)
 #' re <- daFrame(PBMC_fs, PBMC_panel, PBMC_md)
-#' plotMedExprs(re)
+#' 
+#' # run clustering
+#' lineage <- c("CD3", "CD45", "CD4", "CD20", "CD33", 
+#'     "CD123", "CD14", "IgM", "HLA_DR", "CD7")
+#' re <- cluster(re, cols_to_use=lineage)
 #' 
 #' @author
 #' Helena Lucia Crowell \email{crowellh@student.ethz.ch}
@@ -30,13 +34,18 @@
 
 setMethod(f="plotMedExprs", 
     signature=signature(x="daFrame"), 
-    definition=function(x) {
+    definition=function(x, k=20, type2="all", color_by="condition") {
 
+        check_validity_of_k(x, k)
+        
+        if (type2 == "all") t2 <- type2(x) else t2 <- type2
         cluster_ids <- factor(cluster_codes(x)[, k][cluster_ids(x)])
-        med_exprs <- data.frame(
-            exprs(x)[, functional(x)],
+        df <- data.frame(exprs(x)[, t2],
             sample_id=sample_ids(x), 
             cluster_id=cluster_ids)
+        colnames(df)[seq_len(length(t2))] <- t2
+
+        # compute medians across samples & clusters
         med_exprs <- df %>% 
             group_by(sample_id, cluster_id) %>% 
             summarize_all(funs(median))
@@ -44,23 +53,44 @@ setMethod(f="plotMedExprs",
             id.vars=c("sample_id", "cluster_id"),
             variable.name="antigen", 
             value.name="med_expr")
-        med_exprs <- dcast(med_exprs, 
-            cluster_id+antigen~sample_id, 
-            value.var="med_expr")
         
-        
-        ggplot(df, aes_string(x="antigen", y="med_expr", col="condition")) +
-            geom_point(size=2.5, alpha=.75, 
+        # get conditions
+        md <- metadata(x)[[1]]
+        m <- match(med_exprs$sample_id, md$sample_id)
+        conds <- grep("condition", colnames(md), value=TRUE)
+        for (i in seq_along(conds))
+            med_exprs[, conds[i]] <- md[m, conds[i]]
+        if (length(conds) > 1) {
+            conds_combined <- apply(md[, conds], 1, paste, collapse="/")
+            med_exprs$condition <- conds_combined[m]
+        } 
+
+        ggplot(med_exprs, aes_string(x="antigen", y="med_expr", col=color_by)) +
+            facet_wrap(facets="cluster_id", scale="free_y") +
+            geom_point(alpha=.5, 
                 position=position_jitter(width=.25, height=0)) +
             geom_boxplot(width=.75, fill=NA, outlier.color=NA) +
             stat_summary(fun.y="mean", geom="point", 
-                fill="white", size=3, shape=21) +
+                fill="white", size=2.5, shape=21) +
             guides(color=guide_legend(override.aes=list(alpha=1))) +
-            theme_classic() + theme(
+            ylab("median expression") + theme_classic() + theme(
                 panel.grid.major=element_line(color="lightgrey", size=.25),
+                strip.background=element_rect(fill=NA, color=NA),
+                axis.text=element_text(color="black"),
+                axis.text.x=element_text(angle=90, vjust=.5, hjust=1))        
+        
+        ggplot(med_exprs, aes_string(x="cluster_id", y="med_expr", col=color_by)) +
+            facet_wrap(facets="antigen", scale="free_y") +
+            geom_point(alpha=.5, 
+                position=position_jitter(width=.25, height=0)) +
+            geom_boxplot(width=.75, fill=NA, outlier.color=NA) +
+            stat_summary(fun.y="mean", geom="point", 
+                fill="white", size=2.5, shape=21) +
+            guides(color=guide_legend(override.aes=list(alpha=1))) +
+            ylab("Median expression") + theme_classic() + theme(
+                panel.grid.major=element_line(color="lightgrey", size=.25),
+                strip.background=element_rect(fill=NA, color=NA),
                 axis.text=element_text(color="black"),
                 axis.text.x=element_text(angle=90, vjust=.5, hjust=1))
-        
-        
     }
 )
