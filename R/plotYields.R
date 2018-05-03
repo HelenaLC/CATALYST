155d5538ae0c1c0d8e70fb3dfb0e7fa8b03c137d
HelenaLC <crowellh@student.ethz.ch>
2018-05-03 20:20:18 +0200

    + argument 'plotly' for optional interactivity

--- R/plotYields.R
+++ R/plotYields.R
@@ -15,6 +15,8 @@
 #'   character string. If specified, outputs will be generated here.
 #' @param name_ext 
 #'   character string. If specified, will be appended to the plot's name. 
+#' @param plotly
+#'   logical. Should an interactive plot be rendered?
 #'
 #' @details
 #' The overall yield that will be achieved upon application of the specified 
@@ -59,7 +61,8 @@
 
 setMethod(f="plotYields", 
     signature=signature(x="dbFrame"), 
-    definition=function(x, which=0, out_path=NULL, name_ext=NULL) {
+    definition=function(x, which=0, 
+        out_path=NULL, name_ext=NULL, plotly=TRUE) {
         
         ids <- rownames(bc_key(x))
         which <- check_validity_which(which, ids, "yields")
@@ -68,34 +71,34 @@ setMethod(f="plotYields",
         bc_labs <- get_bc_labs(x)
 
         ps <- vector("list", length(which))
+        sep <- ifelse(plotly, "<br>", "\n")
         for (i in seq_along(which)) {
             id <- which[i]
-            ps[[i]] <- plot_yields(
-                id, x, seps, n_bcs, bc_labs)
+            ps[[i]] <- plot_yields(id, x, seps, n_bcs, bc_labs)
             if (length(sep_cutoffs(x)) != 0) {
                 if (id == 0) {
                     p <- paste0(sprintf("%2.2f", sum(yields(x)[cbind(
                         seq_len(n_bcs), findInterval(sep_cutoffs(x), seps))])
                         /n_bcs*100), "%")
+                    ps[[i]] <- ps[[i]] + ggtitle(paste0(p, 
+                        " overall yield", sep, "(with currently set cutoffs)"))
                 } else {
                     p <- paste0(sprintf("%2.2f", yields(x)
                         [id, findInterval(sep_cutoffs(x)[id], seps)]*100), "%")
                     ps[[i]] <- ps[[i]] + ggtitle(paste(bc_labs[ids == id], 
                         paste("(cutoff ", sep_cutoffs(x)[id],
-                        " with", p, "yield)"), sep="<br>"))
+                        " with", p, "yield)"), sep=sep))
                 }
             } else if (id != 0) {
                 ps[[i]] <- ps[[i]] + ggtitle(bc_labs[ids == id])
             }
-            if (id == 0) {
-                ps[[i]] <- ggplotly(ps[[i]], tooltip="text") %>% 
-                    plotly::config(displayModeBar=FALSE)
-                ps[[i]] <- hide_legend(ps[[i]])
-            } else {
-                ps[[i]] <- ggplotly(ps[[i]], 
-                    tooltip=c("Cutoff","Yield","Count")) %>% 
-                    config(displayModeBar=FALSE)
-            }
+            if (plotly)
+                ps[[i]] <- switch(as.character(id),
+                    "0" = hide_legend(ggplotly(ps[[i]], tooltip="text") %>% 
+                            plotly::config(displayModeBar=FALSE)),
+                    ggplotly(ps[[i]], 
+                        tooltip=c("Cutoff","Yield","Count")) %>% 
+                        config(displayModeBar=FALSE))
         }
         if (!is.null(out_path)) {
             outNm <- file.path(out_path, 
@@ -105,3 +108,4 @@ setMethod(f="plotYields",
             ps
         }
     })
+
