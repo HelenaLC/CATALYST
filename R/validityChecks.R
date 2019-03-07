b80abecf09488ace1331a4ea417170d14ea3ba95
HelenaLC <helena@crowells.eu>
2019-03-07 12:09:47 +0100

    add '.' infront of validity check function

--- R/validityChecks.R
+++ R/validityChecks.R
@@ -3,7 +3,7 @@
 #       - stop if not a single ID is valid
 #       - warning if some ID(s) is/are not valid and remove it/them
 # ------------------------------------------------------------------------------
-check_validity_which <- function(which, ids, fct) {
+.check_validity_which <- function(which, ids, fct) {
     
     msg_events <- c(
         " Valid values for 'which' are IDs that occur as row names in the\n",
@@ -59,7 +59,7 @@ check_validity_which <- function(which, ids, fct) {
 #       - a character string that matches with a 'label' specifying
 #         a merging done with 'mergeClusters'
 # ------------------------------------------------------------------------------
-check_validity_of_k <- function(x, k) {
+.check_validity_of_k <- function(x, k) {
     available_clusterings <- colnames(S4Vectors::metadata(x)$cluster_codes)
     is_valid_clustering <- as.character(k) %in% available_clusterings
     if (!is.null(k) && !is_valid_clustering) {
