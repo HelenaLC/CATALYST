7d8770d1247fd3925f641b5e137d0f2d67acef8f
HelenaLC <helena@crowells.eu>
2019-03-08 14:44:18 +0100

    +unit-tests for subsetting&DR

---
+++ tests/testthat/test_daFrame-subsetting.R
@@ -0,0 +1,80 @@
+context("daFrame-subsetting")
+
+library(SingleCellExperiment)
+
+data(PBMC_fs, PBMC_panel, PBMC_md)
+x <- daFrame(PBMC_fs, PBMC_panel, PBMC_md)
+
+test_that("using nothing", {
+    expect_identical(x[ ], x)
+    expect_identical(x[, ], x)
+    expect_error(x[, , ])
+})
+
+test_that("using logicals", {
+    expect_equal(dim(x[TRUE, ]), dim(x))
+    expect_equal(dim(x[, TRUE]), dim(x))
+    expect_equal(dim(x[TRUE, TRUE]), dim(x))
+    
+    expect_equal(dim(x[FALSE, ]), c(0, ncol(x)))
+    expect_equal(dim(x[, FALSE]), c(nrow(x), 0))
+    expect_equal(dim(x[FALSE, FALSE]), c(0, 0))
+    
+    expect_equal(dim(x[TRUE, FALSE]), c(nrow(x), 0))
+    expect_equal(dim(x[FALSE, TRUE]), c(0, ncol(x)))
+
+    l <- as.logical(c(0, 1))
+    ni <- sample(seq(2, nrow(x) - 1), 1)
+    nj <- sample(seq(2, ncol(x) - 1), 1)
+    i <- sample(l, ni, TRUE)
+    j <- sample(l, nj, TRUE)
+    expect_error(x[i, ])
+    expect_error(x[, j])
+    expect_error(x[i, j])
+})
+
+test_that("using numerics", {
+    ni <- 100
+    nj <- 10
+    i <- sample(nrow(x), ni)
+    j <- sample(ncol(x), nj)
+    
+    expect_equal(nrow(x[i, ]), ni)
+    expect_equal(ncol(x[, j]), nj)
+    expect_equal(dim(x[i, j]), c(ni, nj))
+    
+    expect_identical(rowData(x[, j]), rowData(x))
+    expect_identical(colData(x[i, ]), colData(x))
+    
+    expect_identical(rowData(x[i, ]), rowData(x)[i, ])
+    expect_identical(colData(x[, j]), colData(x)[j, ])
+    
+    expect_identical(metadata(x[i, j]), metadata(x))
+})
+
+test_that("col-subsetting doesn't affect DR", {
+    i <- sample(nrow(x), 10)
+    j <- sample(ncol(x), 10)
+    y <- runDR(x, "PCA", i)
+    expect_identical(
+        reducedDim(y, "PCA"),
+        reducedDim(y[, j], "PCA"))
+})
+
+test_that("row-subsetting doesn't affects DR within bonds", {
+    i <- seq_len(10)
+    y <- runDR(x, "PCA", i)
+    expect_identical(
+        reducedDim(y, "PCA"),
+        reducedDim(y[i, ], "PCA"))
+})
+
+test_that("row-subsetting affects DR out of bonds", {
+    n <- 10
+    i1 <- sample(nrow(x), n)
+    i2 <- sample(i1, n-1)
+    y <- runDR(x, "PCA", i1)
+    expect_equivalent(
+        reducedDim(y[i2, ], "PCA"),
+        reducedDim(y, "PCA")[order(i2), ])
+})
