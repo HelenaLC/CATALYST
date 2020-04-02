044b97f89d1192e6c82653582446ce16ee4ea38c
HelenaLC <helena@crowells.eu>
2020-04-02 12:57:07 +0200

    add .get_features to validate and get features to use for plotting/dimred

--- tests/testthat/test_helpers-differential.R
+++ tests/testthat/test_helpers-differential.R
@@ -1,15 +1,30 @@
 context("utilities for differential analysis")
 set.seed(as.numeric(format(Sys.time(), "%s")))
-sce <- .toySCE()
-y <- assay(sce, "exprs")
+x <- .toySCE()
+y <- assay(x, "exprs")
+
+test_that(".get_features", {
+    mcs <- sample(c("type", "state"), nrow(x), TRUE)
+    rowData(x)$marker_class <- mcs
+    expect_identical(.get_features(x, NULL), rownames(x))
+    for (class in c("type", "state"))
+        expect_identical(
+            .get_features(x, class), 
+            get(paste0(class, "_markers"))(x))
+    fs <- sample(rownames(x), 10)
+    expect_identical(.get_features(x, fs), fs)
+    fs[sample(10, 1)] <- "x"
+    expect_error(.get_features(x, "x"))
+    expect_error(.get_features(x, fs))
+})
 
 test_that(".split_cells() by 1 factor", {
     for (by in c("sample_id", "cluster_id")) {
-        cs <- .split_cells(sce, by)
-        expect_identical(names(cs), levels(sce[[by]]))
+        cs <- .split_cells(x, by)
+        expect_identical(names(cs), levels(x[[by]]))
         expect_identical(
             as.numeric(vapply(cs, length, numeric(1))),
-            as.numeric(table(sce[[by]])))
+            as.numeric(table(x[[by]])))
     }
 })
 
@@ -17,10 +32,10 @@ test_that(".split_cells() by 2 factors", {
     for (by in list(
         c("sample_id", "cluster_id"), 
         c("cluster_id", "sample_id"))) {
-        cs <- .split_cells(sce, by)
-        expect_identical(names(cs), levels(sce[[by[1]]]))
+        cs <- .split_cells(x, by)
+        expect_identical(names(cs), levels(x[[by[1]]]))
         expect_true(all(vapply(cs, function(u) 
-            identical(names(u), levels(sce[[by[2]]])), 
+            identical(names(u), levels(x[[by[2]]])), 
             logical(1))))
     }
 })
@@ -28,11 +43,11 @@ test_that(".split_cells() by 2 factors", {
 test_that(".agg() by 1 factor", {
     for (by in c("sample_id", "cluster_id")) {
         for (fun in c("sum", "mean", "median")) {
-            pb <- .agg(sce, by, fun)
+            pb <- .agg(x, by, fun)
             replicate(10, {
-                i <- sample(seq_len(nrow(sce)), 1)
-                j <- sample(levels(sce[[by]]), 1)
-                expect_equal(pb[i, j], get(fun)(y[i, sce[[by]] == j]))
+                i <- sample(seq_len(nrow(x)), 1)
+                j <- sample(levels(x[[by]]), 1)
+                expect_equal(pb[i, j], get(fun)(y[i, x[[by]] == j]))
             })
         }
     }
@@ -43,16 +58,16 @@ test_that(".agg() by 2 factors", {
         c("sample_id", "cluster_id"), 
         c("cluster_id", "sample_id"))) {
         for (fun in c("sum", "mean", "median")) {
-            pb <- .agg(sce, by, fun)
-            expect_identical(names(pb), levels(sce[[by[1]]]))
+            pb <- .agg(x, by, fun)
+            expect_identical(names(pb), levels(x[[by[1]]]))
             expect_true(all(vapply(pb, function(u) 
-                identical(colnames(u), levels(sce[[by[2]]])), 
+                identical(colnames(u), levels(x[[by[2]]])), 
                 logical(1))))
             replicate(10, {
-                a <- sample(levels(sce[[by[1]]]), 1)
-                b <- sample(levels(sce[[by[2]]]), 1)
-                i <- sample(seq_len(nrow(sce)), 1)
-                j <- sce[[by[1]]] == a & sce[[by[2]]] == b
+                a <- sample(levels(x[[by[1]]]), 1)
+                b <- sample(levels(x[[by[2]]]), 1)
+                i <- sample(seq_len(nrow(x)), 1)
+                j <- x[[by[1]]] == a & x[[by[2]]] == b
                 expect_equal(pb[[a]][i, b], get(fun)(y[i, j]))
             })
         }
