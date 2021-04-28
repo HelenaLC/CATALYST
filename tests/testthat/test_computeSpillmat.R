22d39f468610c9ee9c7dfc4a2c3c86cde1118df8
HelenaLC <helena@crowells.eu>
2021-04-28 09:50:44 +0200

    added unit tests for all combis of interactions and method

--- tests/testthat/test_computeSpillmat.R
+++ tests/testthat/test_computeSpillmat.R
@@ -1,48 +1,54 @@
 library(flowCore)
 
-test_that("computeSpillmat()", {
-    # generate some dummy testdata with ss_exp as a template
-    data(ss_exp); set.seed(12345); n_cells <- 100
+# generate some dummy testdata with ss_exp as a template
+data(ss_exp)
+set.seed(12345)
+n_cells <- 100
 
-    # generate a known spillover matrix
-    chs <- colnames(ss_exp)
-    sm <- diag(n_chs <- ncol(ss_exp))
-    colnames(sm) <- rownames(sm) <- chs
-    # add isotope, +/-1, +16 specific spillover
-    sm_ms <- .get_ms_from_chs(chs)
-    sm_mets <- .get_mets_from_chs(chs)
+# generate a known spillover matrix
+chs <- colnames(ss_exp)
+sm <- diag(n_chs <- ncol(ss_exp))
+colnames(sm) <- rownames(sm) <- chs
+# add isotope, +/-1, +16 specific spillover
+sm_ms <- .get_ms_from_chs(chs)
+sm_mets <- .get_mets_from_chs(chs)
 
-    for (i in seq_along(sm_ms)){
-        ch_i <- chs[i]
-        curmass <- sm_ms[i]
-        curmet <- sm_mets[i]
-        iso <- sm_mets == sm_ms[i]
-        p1 <- sm_ms == sm_ms[i] + 1
-        m1 <- sm_ms == sm_ms[i] - 1
-        ox <- sm_ms == sm_ms[i] + 16
-        spill_cols <- chs[(iso | p1 | m1 | ox) & (chs != chs[i])]
-        sm[chs[i], spill_cols] <- runif(length(spill_cols)) / 20
-    }
+for (i in seq_along(sm_ms)) {
+    ch_i <- chs[i]
+    curmass <- sm_ms[i]
+    curmet <- sm_mets[i]
+    iso <- sm_mets == sm_ms[i]
+    p1 <- sm_ms == sm_ms[i] + 1
+    m1 <- sm_ms == sm_ms[i] - 1
+    ox <- sm_ms == sm_ms[i] + 16
+    spill_cols <- chs[(iso | p1 | m1 | ox) & (chs != chs[i])]
+    sm[chs[i], spill_cols] <- runif(length(spill_cols)) / 20
+}
 
-    # generate 100 cells that are positive for each channel
-    mat <- do.call(rbind, replicate(n_cells, diag(n_chs), simplify = FALSE))
+# generate 100 cells that are positive for each channel
+mat <- do.call(rbind, replicate(n_cells, diag(n_chs), simplify = FALSE))
 
-    # add noise & spillover
-    mat <- mat * (rnorm(n_chs * n_cells, 1, 0.01)) * 100
-    mat_sm <- mat %*% sm
+# add noise & spillover
+mat <- mat * (rnorm(n_chs * n_cells, 1, 0.01)) * 100
+mat_sm <- mat %*% sm
 
-    # construct SCE & debarcode
-    sce <- prepData(flowFrame(mat_sm), by_time = FALSE)
-    sce <- assignPrelim(sce, sm_ms, verbose = FALSE)
-    # set barcode IDs to constructed IDs
-    sce$bc_id <- rep(as.character(sm_ms), n_cells)
+# construct SCE & debarcode
+sce <- prepData(flowFrame(mat_sm), by_time = FALSE)
+sce <- assignPrelim(sce, sm_ms, verbose = FALSE)
+
+# set barcode IDs to constructed IDs
+sce$bc_id <- rep(as.character(sm_ms), n_cells)
+    
+for (i in c("default", "all")) 
+    for (m in c("default", "classic")) 
+test_that(sprintf("interactions = '%s', method = '%s'", i, m), {
     # estimate spillover matrix
-    sce <- computeSpillmat(sce)
+    sce <- computeSpillmat(sce, interactions = i, method = m)
     sm_est <- metadata(sce)$spillover_matrix
     expect_equal(sm_est, sm, info = c(
         "test if spillover matrix can be reconstructed",
         "from simulated single cells without noise."))
-
+    
     # repeat with a constant amount of background
     sce <- prepData(flowFrame(mat_sm + 20), by_time = FALSE)
     sce <- assignPrelim(sce, sm_ms, verbose = FALSE)
@@ -52,7 +58,7 @@ test_that("computeSpillmat()", {
     expect_equal(sm_est, sm, info = c(
         "test if spillover matrix can be reconstructed",
         "from simulated single cells without noise but with background."))
-
+    
     noise <- rnorm(n_chs * n_cells, 5, 1)
     sce <- prepData(flowFrame(mat_sm * noise), by_time = FALSE)
     sce <- assignPrelim(sce, sm_ms, verbose = FALSE)
