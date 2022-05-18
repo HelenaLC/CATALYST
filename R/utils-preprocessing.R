1e86a51eb8e95619aeef107a26ea7c55c1d03c83
HelenaLC <helena@crowells.eu>
2022-05-18 12:22:56 +0200

    check for/fix panel discrepancies; expose dots args for read.FCS

--- R/utils-preprocessing.R
+++ R/utils-preprocessing.R
@@ -1,7 +1,43 @@
-
+#' @importFrom flowCore fr_append_cols flowSet
+.match_ffs <- function(ffs, fix_chs) {
+    # ffs <- ffs0[c(1, 2)]
+    # fix_chs <- "all"
+    chs <- lapply(ffs, colnames)
+    if (!all(mapply(identical, chs[1], chs[-1]))) {
+        warning(
+            "Channel names don't match between FCS files; ", 
+            "panel discrepancies will be fixed ", 
+            "keeping ", fix_chs, " channels.")
+        switch(
+            fix_chs, 
+            common = {
+                mtx <- NULL
+                chs_use <- Reduce(intersect, chs)
+                for (i in seq_along(ffs))
+                    ffs[[i]] <- ffs[[i]][, chs_use]
+            }, 
+            all = {
+                n <- length(chs_use <- unique(unlist(chs)))
+                mtx <- vapply(chs, \(.) chs_use %in% ., logical(n))
+                for (i in seq_along(ffs)) {
+                    chs2add <- setdiff(chs_use, chs[[i]])
+                    if (length(chs2add) != 0) {
+                        n <- nrow(ffs[[i]])
+                        y <- vapply(chs2add, \(.) numeric(n), numeric(n))
+                        ffs[[i]] <- fr_append_cols(ffs[[i]], y)
+                    }
+                    ffs[[i]] <- ffs[[i]][, chs_use]
+                }
+            }
+        )
+    }
+    fs <- flowSet(ffs)
+    return(list(fs, mtx))
+}
+                      
 #' @importFrom methods is
-#' @importFrom flowCore isFCSfile read.flowSet
-.read_fs <- function(x) {
+#' @importFrom flowCore isFCSfile read.FCS
+.read_fs <- function(x, fix_chs, ...) {
     if (is(x, "flowFrame")) {
         fs <- flowSet(x)
     } else if (is.list(x)) {
@@ -23,15 +59,20 @@
             fcs <- x  
         }
         stopifnot(all(vapply(fcs, isFCSfile, logical(1))))
-        fs <- read.flowSet(fcs,
-            transformation = FALSE, 
-            truncate_max_range = FALSE)
+        args <- list(...)
+        defs <- c("transformation", "truncate_max_range")
+        for (. in defs) if (is.null(args[[.]])) args[[.]] <- FALSE
+        ffs <- lapply(fcs, \(fnm) do.call(read.FCS, c(fnm, args)))
+        out <- .match_ffs(ffs, fix_chs)
+        fs <- out[[1]]; mtx <- out[[2]]
     } else {
         stop("Invalid argument 'x'; should be either a flowSet",
             " or a character string specifying the path to", 
             " a directory containing a set of FCS files.")
     }
-    return(fs)
+    if (!exists("mtx"))
+        mtx <- NULL
+    return(list(fs, mtx))
 }
 
 #' @importFrom SingleCellExperiment int_metadata<-
